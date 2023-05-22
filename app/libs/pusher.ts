aacee41c8877ff17c51de6a41676bb178127fdc8
herrywidnyana22 <115609431+herrywidnyana22@users.noreply.github.com>
2023-05-23 00:32:58 +0800

    Update pusher.ts
    remove use tls for test

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -5,8 +5,7 @@ export const pusherServer = new PusherServer({
     appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID!,
     key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
     secret: process.env.NEXT_PUBLIC_PUSHER_SECRET!,
-    cluster: 'mt1',
-    useTLS: true
+    cluster: 'mt1'
 })
 
 export const pusherClient = new PusherClient(
